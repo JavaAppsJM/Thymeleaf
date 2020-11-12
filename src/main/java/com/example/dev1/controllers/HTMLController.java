51a9974442bc294b825c743105cb060213650597
Marc J <john.doe.gmail.com>
2020-11-12 16:20:36 +0100

    CSS en js werkt gedeeltelijk

--- src/main/java/com/example/dev1/controllers/HTMLController.java
+++ src/main/java/com/example/dev1/controllers/HTMLController.java
@@ -13,43 +13,50 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/healthmeasurements")
+@RequestMapping("/")// Beter niks zetten want anders word css en js niet gevonden
 public class HTMLController {
     @Autowired
     MeasurementService measurementService;
 
+    // Controller thymeleaf experiments
+    @GetMapping("/healthmeasurements/thymeleaf")
+    public ModelAndView getThyme(ModelMap modelMap) { // ModelMap is like a Hashmap spring will automatically initialize this for you.
+        modelMap.addAttribute("collist", "azerty");
+        return new ModelAndView("thymeleaf", modelMap);
+    }
+
     // Controllers for Belly measurements
-    @GetMapping("/getAllGenBellyMs")
+    @GetMapping("/healthmeasurements/getAllGenBellyMs")
     public ModelAndView getAllGenBellyMs(ModelMap modelMap) { // ModelMap is like a Hashmap spring will automatically initialize this for you.
         modelMap.addAttribute("collist", measurementService.fillIntDisplayWBelly(measurementService.getAllBellyMs()));
         return new ModelAndView("measurementDisplay", modelMap);
     }
 
-    @GetMapping("/addGenBellyM")
+    @GetMapping("/healthmeasurements/addGenBellyM")
     public ModelAndView addGenBellyM(ModelMap modelMap) {
         modelMap.addAttribute("GenMap", measurementService.fillIntEditWBelly(new BellyMesurement(), "add"));
         return new ModelAndView("measurementEdit", modelMap);
     }
 
-    @GetMapping("/{id}/editGenBellyM") // <---- Creates url in the form of localhost:port/healthmesurements/{id}/edit
+    @GetMapping("/healthmeasurements/{id}/editGenBellyM") // <---- Creates url in the form of localhost:port/healthmesurements/{id}/edit
     public ModelAndView editGenBellyM(@PathVariable("id") int id, ModelMap modelMap) {
         modelMap.addAttribute("GenMap", measurementService.fillIntEditWBelly(measurementService.findById(id), "edit"));
         return new ModelAndView("measurementEdit", modelMap);
     }
 
-    @GetMapping("/{id}/deleteGenBellyM")
+    @GetMapping("/healthmeasurements/{id}/deleteGenBellyM")
     public ModelAndView deleteGenBellyM(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
         measurementService.deleteById(id);
         return new ModelAndView("redirect:/healthmeasurements/getAllGenBellyMs");
     }
 
-    @PostMapping("/editGenBellyM")
+    @PostMapping("/healthmeasurements/editGenBellyM")
     public ModelAndView editPostGenBellyM(@ModelAttribute HTMLIntEdit template) {
         measurementService.update(measurementService.moveIntEditInBelly(template));
         return new ModelAndView("redirect:/healthmeasurements/getAllGenBellyMs");
     }
 
-    @PostMapping("/addGenBellyM")
+    @PostMapping("/healthmeasurements/addGenBellyM")
     public ModelAndView addPostGenBellyM(@ModelAttribute HTMLIntEdit template) {
         measurementService.addBellyM(measurementService.moveIntEditInBelly(template));
         return new ModelAndView("redirect:/healthmeasurements/getAllGenBellyMs");
@@ -59,41 +66,39 @@ public class HTMLController {
     // Controllers for Blood pressure measurements ====================================================================
     // ================================================================================================================
 
-    @GetMapping("/getAllGenBPresMs")
+    @GetMapping("/healthmeasurements/getAllGenBPresMs")
     public ModelAndView getAllGenBPresMs(ModelMap modelMap) { // ModelMap is like a Hashmap spring will automatically initialize this for you.
         modelMap.addAttribute("collist", measurementService.fillIntDisplayWBPres(measurementService.getAllBPresMs()));
         return new ModelAndView("measurementDisplay", modelMap);
     }
 
-    @GetMapping("/addGenBPresM")
+    @GetMapping("/healthmeasurements/addGenBPresM")
     public ModelAndView addGenBPresM(ModelMap modelMap) {
         modelMap.addAttribute("GenMap", measurementService.fillIntEditWBPres(new BloodPressureMesurement(), "add"));
         return new ModelAndView("measurementEdit", modelMap);
     }
 
-    @PostMapping("/addGenBPresM")
+    @PostMapping("/healthmeasurements/addGenBPresM")
     public ModelAndView addPostGenBPresM(@ModelAttribute HTMLIntEdit template) {
-        measurementService.addBPresM(measurementService.moveTemplateInBPres(template));
+        measurementService.addBPresM(measurementService.moveIntEditInBPres(template));
         return new ModelAndView("redirect:/healthmeasurements/getAllGenBPresMs");
     }
 
-    @GetMapping("/{id}/editGenBPresM") // <---- Creates url in the form of localhost:port/healthmesurements/{id}/edit
+    @GetMapping("/healthmeasurements/{id}/editGenBPresM") // <---- Creates url in the form of localhost:port/healthmesurements/{id}/edit
     public ModelAndView editGenBPresM(@PathVariable("id") int id, ModelMap modelMap) {
         modelMap.addAttribute("GenMap", measurementService.fillIntEditWBPres(measurementService.findBPresById(id), "edit"));
         return new ModelAndView("measurementEdit", modelMap);
     }
 
-    @GetMapping("/{id}/deleteGenBPresM")
+    @GetMapping("/healthmeasurements/{id}/deleteGenBPresM")
     public ModelAndView deleteGenBPresM(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
         measurementService.deleteBPresById(id);
         return new ModelAndView("redirect:/healthmeasurements/getAllGenBPresMs");
     }
 
-    @PostMapping("/editGenBPresM")
+    @PostMapping("/healthmeasurements/editGenBPresM")
     public ModelAndView editPostGenBPresM(@ModelAttribute HTMLIntEdit template) {
         measurementService.updateBPres(measurementService.moveIntEditInBPres(template));
         return new ModelAndView("redirect:/healthmeasurements/getAllGenBPresMs");
     }
-
-
 }
--- src/main/java/com/example/dev1/services/MeasurementService.java
+++ src/main/java/com/example/dev1/services/MeasurementService.java
@@ -64,34 +64,6 @@ public class MeasurementService {
         return hColTemplate;
     }
 
-    public HTMLColumnListD fillBellyColTemplate(List<BellyMesurement> bellyMesurementList){
-        String[] colList = new String[4];
-        colList[0] = "Measurement Id";
-        colList[1] = "Measurement Date";
-        colList[2] = "Circumreference";
-        colList[3] = "Actions";
-        String[][] displayList = new String[bellyMesurementList.size()][3];
-        int i = 0;
-        int j = 0;
-        for (BellyMesurement b: bellyMesurementList) {
-            displayList[i][j] = String.valueOf(b.getMesureId());
-            j++;
-            displayList[i][j] = String.valueOf(b.getMesureDate());
-            j++;
-            displayList[i][j] = String.valueOf(b.getCircumRef());
-            j = 0;
-            i++;
-        }
-
-        HTMLColumnListD hColTemplate = new HTMLColumnListD();
-        hColTemplate.sethList(colList);
-        hColTemplate.sethDisplayList(displayList);
-        hColTemplate.sethTitle("Display Belly Measurements");
-        hColTemplate.sethMType("BellyM");
-
-        return hColTemplate;
-    }
-
     public HTMLIntEdit fillIntEditWBelly(BellyMesurement bellyMesurement, String action){
         // Prepare formfields
         String[][] tempList = new String[1][3];
@@ -117,43 +89,7 @@ public class MeasurementService {
         return htmlTemplate;
     }
 
-    public HTMLTemplate fillBellyInHtmlTemplate(BellyMesurement bellyMesurement, String action){
-        String[][] tempList = new String[1][3];
-        tempList[0][0] = "Circumreference";
-        tempList[0][1] = "circumRef";
-        tempList[0][2] = String.valueOf(bellyMesurement.getCircumRef());
-        HTMLTemplate htmlTemplate = new HTMLTemplate();
-        String pTitle = action.toUpperCase() + " Belly Measurement";
-        htmlTemplate.sethTitle(pTitle);
-        String formActURL = "/healthmeasurements/" + action + "GenBellyM";
-        htmlTemplate.sethFormActionUrl(formActURL);
-        htmlTemplate.sethList(tempList);
-        htmlTemplate.setCircumRef(bellyMesurement.getCircumRef());
-        htmlTemplate.setMesureId(bellyMesurement.getMesureId());
-        if (bellyMesurement.getMesureDate() == null){
-            bellyMesurement.setMesureDate(LocalDate.now());
-        }
-        htmlTemplate.setDay(bellyMesurement.getMesureDate().getDayOfMonth());
-        htmlTemplate.setMonth(bellyMesurement.getMesureDate().getMonthValue());
-        htmlTemplate.setYear(bellyMesurement.getMesureDate().getYear());
-        return htmlTemplate;
-    }
-
     public BellyMesurement moveIntEditInBelly(HTMLIntEdit template){
-        BellyMesurement bellyMesurement = new BellyMesurement();
-        bellyMesurement.setMesureId(template.getMesureId());
-        if (template.getDay() == 0  || template.getMonth() == 0 || template.getYear() == 0){
-            bellyMesurement.setMesureDate(LocalDate.now());
-        } else {
-            bellyMesurement.setMesureDate
-                    (LocalDate.of(template.getYear(),template.getMonth(),template.getDay()));
-        }
-        bellyMesurement.setCircumRef(template.getCircumRef());
-
-        return bellyMesurement;
-    }
-
-    public BellyMesurement moveTemplateInBelly(HTMLTemplate template){
         BellyMesurement bellyMesurement = new BellyMesurement();
         bellyMesurement.setMesureId(template.getMesureId());
         if (template.getDay() == 0  || template.getMonth() == 0 || template.getYear() == 0){
@@ -226,40 +162,6 @@ public class MeasurementService {
         return hColTemplate;
     }
 
-    public HTMLColumnListD fillBPresColTemplate(List<BloodPressureMesurement> bloodPressureMesurementList){
-        String[] colList = new String[6];
-        colList[0] = "Measurement Id";
-        colList[1] = "Measurement Date";
-        colList[2] = "Heart Beat";
-        colList[3] = "Blood Pressure High";
-        colList[4] = "Blood Pressure Low";
-        colList[5] = "Actions";
-        String[][] displayList = new String[bloodPressureMesurementList.size()][5];
-        int i = 0;
-        int j = 0;
-        for (BloodPressureMesurement b: bloodPressureMesurementList) {
-            displayList[i][j] = String.valueOf(b.getMesureId());
-            j++;
-            displayList[i][j] = String.valueOf(b.getMesureDate());
-            j++;
-            displayList[i][j] = String.valueOf(b.getHeartBeat());
-            j++;
-            displayList[i][j] = String.valueOf(b.getBloodPressureHigh());
-            j++;
-            displayList[i][j] = String.valueOf(b.getBloodPressureLow());
-            j = 0;
-            i++;
-        }
-
-        HTMLColumnListD hColTemplate = new HTMLColumnListD();
-        hColTemplate.sethList(colList);
-        hColTemplate.sethDisplayList(displayList);
-        hColTemplate.sethTitle("Display Blood Pressure Measurements");
-        hColTemplate.sethMType("BPresM");
-
-        return hColTemplate;
-    }
-
     public HTMLIntEdit fillIntEditWBPres(BloodPressureMesurement measurement, String action){
         // Prepare formfields
         String[][] tempList = new String[3][3];
@@ -293,53 +195,7 @@ public class MeasurementService {
         return htmlTemplate;
     }
 
-    public HTMLTemplate fillBPresInHtmlTemplate(BloodPressureMesurement measurement, String action){
-        String[][] tempList = new String[3][3];
-        tempList[0][0] = "Heart Beat";
-        tempList[0][1] = "HeartBeat";
-        tempList[1][0] = "Blood Upper Pressure";
-        tempList[1][1] = "bloodPressureHigh";
-        tempList[2][0] = "Blood Lower Pressure";
-        tempList[2][1] = "bloodPressureLow";
-        tempList[0][2] = String.valueOf(measurement.getHeartBeat());
-        tempList[1][2] = String.valueOf(measurement.getBloodPressureHigh());
-        tempList[2][2] = String.valueOf(measurement.getBloodPressureLow());
-        HTMLTemplate htmlTemplate = new HTMLTemplate();
-        String pTitle = action.toUpperCase() + " Blood Pressure Measurement";
-        htmlTemplate.sethTitle(pTitle);
-        String formActURL = "/healthmeasurements/" + action + "GenBPresM";
-        htmlTemplate.sethFormActionUrl(formActURL);
-        htmlTemplate.sethList(tempList);
-        htmlTemplate.setHeartBeat(measurement.getHeartBeat());
-        htmlTemplate.setBloodPressureHigh(measurement.getBloodPressureHigh());
-        htmlTemplate.setBloodPressureLow(measurement.getBloodPressureLow());
-        htmlTemplate.setMesureId(measurement.getMesureId());
-        if (measurement.getMesureDate() == null){
-            measurement.setMesureDate(LocalDate.now());
-        }
-        htmlTemplate.setDay(measurement.getMesureDate().getDayOfMonth());
-        htmlTemplate.setMonth(measurement.getMesureDate().getMonthValue());
-        htmlTemplate.setYear(measurement.getMesureDate().getYear());
-
-        return htmlTemplate;
-    }
-
     public BloodPressureMesurement moveIntEditInBPres(HTMLIntEdit template){
-        BloodPressureMesurement measurement = new BloodPressureMesurement();
-        measurement.setMesureId(template.getMesureId());
-        if (template.getDay() == 0  || template.getMonth() == 0 || template.getYear() == 0){
-            measurement.setMesureDate(LocalDate.now());
-        } else {
-            measurement.setMesureDate
-                    (LocalDate.of(template.getYear(),template.getMonth(),template.getDay()));
-        }
-        measurement.setHeartBeat(template.getHeartBeat());
-        measurement.setBloodPressureHigh(template.getBloodPressureHigh());
-        measurement.setBloodPressureLow(template.getBloodPressureLow());
-
-        return measurement;
-    }
-    public BloodPressureMesurement moveTemplateInBPres(HTMLIntEdit template){
         BloodPressureMesurement measurement = new BloodPressureMesurement();
         measurement.setMesureId(template.getMesureId());
         if (template.getDay() == 0  || template.getMonth() == 0 || template.getYear() == 0){
